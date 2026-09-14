4d4f3581c5f066098c6a58c76fa1474df2c4f9c6
Timon Schreiber <timon.schreiber1@smail.int.h-brs.de>
2026-09-14 13:20:22 +0200

    Copy flight state translation into :state-model

---
+++ state-model/src/main/kotlin/org/WenuLink/adapters/aircraft/TelemetryData.kt
@@ -0,0 +1,7 @@
+package org.WenuLink.adapters.aircraft
+
+class TelemetryData {
+    // Class Stub for now to not have to change the file name
+}
+
+data class Coordinates3D(val lat: Double, val long: Double, val alt: Float)
